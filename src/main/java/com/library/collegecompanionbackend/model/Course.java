7d07b060483e3f134921a9535c5713566ccb8ad0
JatinVijay08 <jatinvijayvargiya0805@gmail.com>
2025-12-17 15:51:16 +0530

    Added Course entity and Mapped User with Many to One Mapping

--- src/main/java/com/library/collegecompanionbackend/model/Course.java
+++ src/main/java/com/library/collegecompanionbackend/model/Course.java
@@ -6,30 +6,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false,unique = true)
-    private String email;
-
-    @Enumerated(EnumType.STRING)
+    private String courseName;
     @Column(nullable = false)
-    private Role role;
+    private String courseCode;
+    @Column(nullable = false)
+    private String courseDescription;
 
-    @OneToMany(mappedBy = "createdBy")
-    List<Course> coursesByTeacher;
-
+    @ManyToOne
+    @JoinColumn(name="teacher_id",nullable = false)
+    private User teacher;
 
 }
